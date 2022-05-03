b0f2348b0080396922e342a61aaba71863257aad
minwoo <peropero502321@gmail.com>
2022-05-03 10:41:52 +0900

    이미지 import로 가져오기

--- src/Section.js
+++ src/Section.js
@@ -1,9 +1,25 @@
+import pic1 from './img/Blizzards.jpg';
+import pic2 from './img/Calm.jpg';
+import pic3 from './img/Dusty_Road.jpg';
+import pic4 from './img/Escape.jpg';
+import pic5 from './img/Payday.jpg';
+import pic6 from './img/Retreat.jpg';
+import pic7 from './img/Seasonal.jpg';
+import pic8 from './img/Vespers.jpg';
+
+
 function Section (){
-  const arr = ['bizzards','calm', 'dusty_road','escape','patday','retreact','seasonal','vespoers'];
+  const imgs = [pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8];
+  const arr = ['Bizzards','Calm', 'Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
   return (
     <section className="wrap">
       {arr.map((data,idx)=>{
-        return <article key={idx}>{data}</article>
+        return (
+        <article key={idx}>
+          <p>{data}</p>
+          <img src={imgs[idx]} alt="" />
+        </article>
+        )
       })}
     </section>
   )
