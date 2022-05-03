ac5e41d6ff58d30475d78974805c829cb3a8b7e6
minwoo <peropero502321@gmail.com>
2022-05-03 11:05:03 +0900

    public에서 이미지 가져오기

--- src/Section.js
+++ src/Section.js
@@ -1,15 +1,8 @@
-import pic1 from './img/Blizzards.jpg';
-import pic2 from './img/Calm.jpg';
-import pic3 from './img/Dusty_Road.jpg';
-import pic4 from './img/Escape.jpg';
-import pic5 from './img/Payday.jpg';
-import pic6 from './img/Retreat.jpg';
-import pic7 from './img/Seasonal.jpg';
-import pic8 from './img/Vespers.jpg';
 
+const path = process.env.PUBLIC_URL;
 
 function Section (){
-  const imgs = [pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8];
+
   const arr = ['Bizzards','Calm', 'Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
   return (
     <section className="wrap">
@@ -17,7 +10,7 @@ function Section (){
         return (
         <article key={idx}>
           <p>{data}</p>
-          <img src={imgs[idx]} alt="" />
+          <img src={`${path}/img/${data}.jpg`} />
         </article>
         )
       })}
