e9aebd8c1483507b518861eb7e04ae764d5a9ce4
minwoo <peropero502321@gmail.com>
2022-05-03 12:08:42 +0900

    jsx로 스타일 객체 연결

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,9 @@
 function Card(props){
-
+  console.log(props)
+  const style = {transform:`rotate(${360/props.num * props.index}deg) translateY(-150%)`}
+  console.log(style)
   return (
-    <article>
+    <article style={style}>
       <div className='inner'>
         <div className='pic'>
           <img src={`${props.path}/img/${props.data}.jpg`} />  
