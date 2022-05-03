123ad8e72e7a7bd377783837c2a84f153f991e04
minwoo <peropero502321@gmail.com>
2022-05-03 11:28:55 +0900

    prop을 활용하여 데이터값 전달

--- src/Section.js
+++ src/Section.js
@@ -1,18 +1,15 @@
 
+import Card from './Card';
+
 const path = process.env.PUBLIC_URL;
 
 function Section (){
 
-  const arr = ['Bizzards','Calm', 'Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
+  const arr = ['Blizzards','Calm', 'Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
   return (
     <section className="wrap">
       {arr.map((data,idx)=>{
-        return (
-        <article key={idx}>
-          <p>{data}</p>
-          <img src={`${path}/img/${data}.jpg`} />
-        </article>
-        )
+        return <Card key={idx} data={data} path={path} />
       })}
     </section>
   )
