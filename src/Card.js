391d1200b0a2cdf71ee4604fc5c20161fba3d22a
minwoo <peropero502321@gmail.com>
2022-05-03 12:45:05 +0900

    useState를 활요한 state변경 및 화면 재랜더링

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 function Card(props){
-  console.log(props)
+  // console.log(props)
   const style = {transform:`rotate(${360/props.num * props.index}deg) translateY(-150%)`}
-  console.log(style)
   return (
     <article style={style}>
       <div className='inner'>
--- src/Section.js
+++ src/Section.js
@@ -1,19 +1,26 @@
 
 import Card from './Card';
+import {useState} from 'react';
 
 const path = process.env.PUBLIC_URL;
 
 function Section (){
-
+  console.log('section 함수 재호출');
   const arr = ['Blizzards','Calm', 'Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
   const num = arr.length;
+  let [index, setIndex] = useState(0);
+  console.log(index)
 
   return (
-    <section className="wrap" style={{top:'120%'}}>
-      {arr.map((data,idx)=>{
-        return <Card key={idx} data={data} path={path} num={num} index={idx} />
-      })}
-    </section>
+    <>
+      <section className="wrap" style={{transform:`rotate(${45*index}deg)`}}>
+        {arr.map((data,idx)=>{
+          return <Card key={idx} data={data} path={path} num={num} index={idx} />
+        })}
+      </section>
+      <a href="#" className="prev" onClick={()=>{setIndex(++index)}}></a>
+      <a href="#" className="next" onClick={()=>{setIndex(--index)}}></a>
+    </>
   )
 }
 export default Section;
